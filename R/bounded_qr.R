5f77a6d8878ddd8b33b3866dbca11a1bc826e248
Blake Boswell <j.blake.boswell@gmail.com>
2018-03-03 03:01:22 -0500

    implement initial r class for bounded_qr

--- R/bounded_qr.R
+++ R/bounded_qr.R
@@ -1,38 +1,19 @@
 
-#' #' @internal
-#' bounded_qr <- R6::R6Class(
-#'   "bounded_qr",
-#'   cloneable = FALSE,
-#'   portable  = FALSE,
-#'   public = list(
-#'     qr = NULL,
-#'     initialize = function(np) {
-#'       self$qr <- new(Class = BoundedQr, np)
-#'     }
-#'   )
-#' )
-
-
-
 #' @internal
-new_bounded_qr <- function(np) {
-  qr <- new(Class = BoundedQr, np)
-  e        <- environment()
-  class(e) <- "bounded_qr"
-  e
+new_bounded_qr <- function(p) {
+  new(Class = BoundedQr, p)
 }
 
 
 #' @export 
 #'
-#' @param qr bounded_qr object to update
+#' @param qr Rcpp_BoundedQr object to update
 #' @param X matrix of covariate observations
 #' @param y response
 #' @param weights weights of each observation
-update.bounded_qr <- function(obj, X, y, weights) {
-  # error check
-  obj$qr$update(X, y, weights)
-  obj
+update.Rcpp_BoundedQr <- function(qr, X, y, weights) {
+  qr$update(X, y, weights)
+  qr
 }
 
 
