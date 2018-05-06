af66bb6ded5a329c269d769802223ac18546d516
Blake Boswell <j.blake.boswell@gmail.com>
2018-05-06 17:10:27 -0400

    simple connection and data.frame feed implementation

--- R/feed.R
+++ R/feed.R
@@ -1,32 +1,82 @@
-  
-oomfeed <- function(data,
-                    chunksize = 5000) {
-  
+
+#' @export
+oomfeed <- function(obj, chunksize, ...){
+  UseMethod("oomfeed")
+}
+setGeneric("oomfeed")
+
+
+#' @export
+oomfeed.data.frame <- function(conn, chunksize, ...) {
+
   reset  <- FALSE
-  n      <- nrow(data)
+  n      <- nrow(conn)
   cursor <- 0
-  
+
   function() {
-    
+
     if (reset) {
       cursor <<- 0
       reset  <<- FALSE
       return(NULL)
     }
-    
+
     if (cursor >= n) {
       return(NULL)
     }
-      
+
     start  <- cursor + 1
     cursor <<- cursor + min(chunksize, n - cursor)
-    
+
     if(cursor == n) {
       reset <<- TRUE
     }
-    
-    data[start:cursor, ]
-    
+
+    conn[start:cursor, ]
+
+  }
+
+}
+
+
+
+#' @export
+oomfeed.connection <- function(conn, chunksize, ...) {
+
+  reset <- FALSE
+  deats <- summary(conn)
+  
+  conn_fxn <- function(class_name){
+    switch(class_name, "file" = file, "url-libcurl" = url, NULL)
   }
   
+  if(is.null(fxn <- conn_fxn(deats$class))) {
+    stop("unsuppored connection")
+  }
+  
+  close(conn)
+  conn <- fxn(deats$description, open = deats$mode)
+  
+  function() {
+
+    if(reset) {
+      if(!is.null(conn)) {
+        close(conn)
+      }
+      conn  <<- fxn(deats$description, open = deats$mode)
+      reset <<- FALSE
+    }
+
+    rval <- read.table(conn, nrows = chunksize, ...)
+
+    if(nrow(rval) == 0) {
+      close(conn)
+      reset <<- TRUE
+      conn  <<- NULL
+      return(NULL)
+    }
+
+    rval
+
+  }
 }
